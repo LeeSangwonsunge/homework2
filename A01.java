5793928180854e26c564b903d86dce553c2e3bac
LeeSangwon <zzxng123@naver.com>
2016-09-23 16:21:04 +0900

    생일파일 수정

--- A01.java
+++ A01.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
-class A01 {
+class A01 
+	{
+	int age=0;
 	public static void main(String[] s){
 
 		new A01().start();
@@ -8,16 +10,16 @@ class A01 {
 
 		void start(){
 			Scanner s = new Scanner(System.in);
-			System.out.print("태어난 년도를 입력하세요");
-			int birth = s.nextInt;
+			System.out.print("your birth_year ");
+			int birth = s.nextInt();
 			this.age = 2016 - birth + 1;
 			print();
 		}
 
 		void print(){
 			if(this.age < 20)
-				System.out.println("미성년자 입니다.");
+				System.out.println("children");
 			else
-				System.out.println("미성년자가 아닙니다.");
+				System.out.println("adult");
 		}
 }
