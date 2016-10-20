453849437a6f37c4d08071762cbd357520628609
LeeSangwonsunge <zzxng123@naver.com>
2016-10-21 00:23:28 +0900

    Update A02.java

--- A02.java
+++ A02.java
@@ -1,22 +1,22 @@
 import java.util.*;
 class A02{
-        void input(){
-	                Scanner s = new Scanner(System.in);
-			                System.out.print(" input_degree : ");
-					                double input_degree = s.nextDouble();
-							                System.out.print(" kind : ");
-									               
-											                double output_degree = 0;
-													                if(kind.equals("c")){
-																		output_degree=input_degree * 1.8 +32;
-																					System.out.println(output_degree);
-																							}
-																									else if(kind.equals("f")){
-																												output_degree=(input_degree - 32)/1.8;
-																															System.out.println(output_degree);
-																																	}
-																																	        }
-																																		        public static void main(String[] args){
-																																			                new A02().input();
-																																					        }
-																																						}
+	void input(){
+		Scanner s = new Scanner(System.in);
+		System.out.print(" input_degree : ");
+		double input_degree = s.nextDouble();
+		System.out.print(" kind : ");
+		String kind = s.next();
+		double output_degree = 0;
+		if(kind.equals("c")){
+			output_degree=input_degree * 1.8 +32;
+			System.out.println(output_degree);
+		}
+		else if(kind.equals("f")){
+			output_degree=(input_degree - 32)/1.8;
+			System.out.println(output_degree);
+		}
+	}
+	public static void main(String[] args){
+		new A02().input();
+	}
+}
