ee4b7685fc0bcd00ddaf6015e0a344a60a616219
LeeSangwonsunge <zzxng123@naver.com>
2016-10-21 00:07:47 +0900

    Update A01.java

--- A01.java
+++ A01.java
@@ -1,16 +1,16 @@
 import java.util.*;
 class A01{
-        void input(){
-	                Scanner s = new Scanner(System.in);
-			                System.out.print(" your birth year : ");
-					                int birth = s.nextInt();
-							                int age = 2016 - birth + 1;
-									                if(age>19)
-														System.out.println("adult");
-																else
-																			System.out.println("child");
-																			        }
-																				        public static void main(String[] args){
-																					                new A01().input();
-																							        }
-																								}
+	void input(){
+		Scanner s = new Scanner(System.in);
+		System.out.print(" your birth year : ");
+		int birth = s.nextInt();
+		int age = 2016 - birth + 1;
+		if(age>19)
+			System.out.println("adult");
+		else
+			System.out.println("child");
+	}
+	public static void main(String[] args){
+		new A01().input();
+	}
+}
